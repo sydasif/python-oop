826add483d681dc912631607f658eeca518e0e2b
Syed Asif <sydasif78@gmail.com>
2024-09-03 11:44:10 +0500

    Add comments 01_OOP

--- corey/01_OOP.py
+++ corey/01_OOP.py
@@ -1,7 +1,11 @@
-# Python Object-Oriented Programming
+# Understanding Classes, Attributes, and Instances in Python
 
 
+# Define a class named Employee
 class Employee:
+    # The __init__ method is a special method called when an object is
+    # created. It initializes the attributes of the class with values
+    # provided during the creation of an object.
     def __init__(self, name, last, pay):
         self.name = name
         self.last = last
@@ -9,8 +13,10 @@ class Employee:
         self.email = self.name + "." + last + "@abc.com"
 
 
+# Create instances (objects) of the Employee class with different attributes
 emp_1 = Employee("John", "Doe", 50000)
 emp_2 = Employee("Test", "User", 6000)
 
+# Print the email address for each employee instance
 print(emp_1.email)
 print(emp_2.email)
