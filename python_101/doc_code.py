3eb76450f0a7e7d86e2be5dbfd79891f04e4e307
Syed Asif <sydasif78@gmail.com>
2024-09-04 20:11:42 +0500

    Python doc

---
+++ python_101/doc_code.py
@@ -0,0 +1,18 @@
+# Example of a comment
+num = 10
+
+# In-line comment
+x = 10  # 10 is being assigned to x
+
+# Multiline comments
+"""This is a
+multiline comment"""
+
+"""This is also a
+multiline comment"""
+
+
+# Docstring example
+def my_function():
+    """This is the function's docstring"""
+    pass
